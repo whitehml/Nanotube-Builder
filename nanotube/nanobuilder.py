5dc10bcaef41e9c9db12f46d860cac7c0c3f9e8b
Gawquon <michael.l.whitehurst@vanderbilt.edu>
2018-08-03 12:23:40 -0500

    unit cell folding

--- nanotube/nanobuilder.py
+++ nanotube/nanobuilder.py
@@ -31,3 +31,23 @@ n,m : Chriality parameters
                     c.translate((sqrt(3)*unit_length*i,0,0))
                 else:
                     c.translate((sqrt(3)*unit_length*i,unit_length,0))
+                    
+        cell_2 = deepcopy(cell)
+        mb.rotate(cell_2, pi, around=np.asarray([1, 0, 0]))
+        cell_2.translate((0,-.142,0))
+
+        unit_cell = mb.Compound()
+        unit_cell.add(cell)
+        unit_cell.add(cell_2)
+        unit_cell.translate((0,.213,0))
+
+        #Fold the unit cell
+        circumference = .246 * n
+
+        for C in unit_cell:
+            x_index = (C.pos[0]/circumference)*2*pi
+            x = cos(x_index) * (circumference/(2*pi))
+            y = sin(x_index) * (circumference/(2*pi))
+            z = C.pos[1]
+            
+            C.translate_to((x,y,z))
