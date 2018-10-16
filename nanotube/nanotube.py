070424c05210deb580bebfa51f3b2926212081d9
Gawquon <michael.l.whitehurst@vanderbilt.edu>
2018-10-16 15:13:55 -0500

    setup logic and class reorganization

--- nanotube/nanotube.py
+++ nanotube/nanotube.py
@@ -5,31 +5,6 @@ from math import *
 
 __all__ = ['SWCNT']
 
-# define repeatable row units for the different chiralities
-class armchair(mb.Compound):
-    def __init__(self):
-        super(armchair,self).__init__()
-        
-        for i in range(0,4):
-            self.add(C())
-
-        self[1].translate((.071,.071*np.sqrt(3),0))
-        self[2].translate((.213,.071*np.sqrt(3),0))
-        self[3].translate((.284,0,0))
-
-class zigzag(mb.Compound):
-    def __init__(self,orientation=1):
-        super(zigzag,self).__init__()
-
-        if orientation == 0:
-            raise ValueError("Orientation must either be set to some positive value or some negative value")
-        
-        for i in range(0,3):
-            self.add(C())
-
-        self[1].translate((.071*np.sqrt(3),.071*orientation,0))
-        self[2].translate((2*.071*np.sqrt(3),0,0))
-
 class SWCNT(mb.Compound):
 """ A single-walled Carbon nanotube recipe
 radius: radius of nanotube in nm
@@ -39,10 +14,29 @@ chirality : If using radius instead of n and m, the type of chirality desired
 n,m : chirality parameters, if only n is defined armchair is assumed
 length : length of nanotube in nm
 
-note: either the radius or the chiraity parameters must be defined but not both. (Should let radius override and provide warning message)
+note: either the radius or the chiraity parameters must be defined but not
+both.
 """
     def __init__(self,radius=None,chirality="armchair",n=None,m=None,length):
         super(SWCNT, self).__init__()
+
+        # define repeatable row units for the different chiralities
+        _ARMCHAIR = np.array([(0,0,0)
+                    (.071,.071*np.sqrt(3),0)
+                    (.213,.071*np.sqrt(3),0)
+                    (.284,0,0)])
+
+        _ARM_WALK = .426
+
+        _ZIGZAG = np.array([(0,0,0)
+                    (.071*np.sqrt(3),.071,0)])
+
+        _ZIG_WALK = 2*.071*np.sqrt(3)
+
+        _CHIRAL_UNIT_DICT = {'armchair':_ARMCHAIR, 'zigzag':_ZIGZAG}
+        _CHIRAL_LENGTH_DICT = {'armchair':_ARM_WALK, 'zigzag':_ZIG_WALK}
+
+        _ORIENTATION = {'armchair': 1, 'zigzag': -1}
 
         # define Carbon atom
         class C(mb.Compound):
@@ -53,22 +47,48 @@ note: either the radius or the chiraity parameters must be defined but not both.
 
         #Checking validity of input values
         if radius is None and n is None:
-            raise ValueError("Either the radius or the chiraity parameters must be defined")
+            raise ValueError("Either the radius or the chiraity parameters"
+            "must be defined")
         elif radius is not None and n is not None:
-            raise RuntimeWarning("Both radius and chirality parameters defined,radius overriding (n,m) values")
+            raise RuntimeWarning("Both radius and chirality parameters defined,"
+            "radius overriding (n,m) values")
+
+            n = None
+            m = None
+
         elif radius < 0.19:
-            raise ValueError("The smallest possible radius is 0.19 nm. Your radius was " + str(radius) + " nm."
+            raise ValueError("The smallest possible radius is 0.19 nm. Your"
+            " radius was " + str(radius) + " nm.")
+        elif m is not None and (m is not n or m is not 0):
+            raise ValueError("m must equal n or 0. Chiral nanotubes not"
+            "currently supported")
+
+        if radius is not None and chirality is not in ['armchair','zigzag']:
+            raise ValueError("Chirality must be either \'armchair\' or"
+            "\'zigzag\'")
+
 
         #Sets implied chirality from the n,m values if nessecary
-        if n is not None and m is 0 and radius is None:
+        if n is not None and m is 0:
             chirality = "zigzag"
 
-        # Next calculate real radius and find the sheet dimensions in terms of
-        # different row units. Change armchair and zigzag classes to just 
-        # record carbon positions and perform calculations on that. Do not
-        # populate with actual Compound objects until final positions are
-        # recorded.
-        # 
+        #Find number of cells in row and number of rows in tube
+        if radius is None:
+            n_cells = n
+        else:
+            #Divides circumference of the desired nanotube by the space taken
+            # up by one repeatable unit for specified chirality  
+            n_cells_aprx = nano_radius*(np.pi*2)/_CHIRAL_LENGTH_DICT[chirality]
+
+            n_cells = int(round(n_cells_aprx))
+
+        #Finds number of rows in tube
+        z = int(round((length-.071)/.213) + 1) # WIP
+
+        #Propagate a row
+        
+
+
         # Armchair/zigzag unit cell -> full rows -> full sheets -> folded tubes 
         # -> insert C atoms
         # 
